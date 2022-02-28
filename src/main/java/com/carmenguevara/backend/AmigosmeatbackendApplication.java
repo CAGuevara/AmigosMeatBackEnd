434b197f65ddf1dc7bc9bdda78dce1130f94e94c
Carmen Guevara <carmen.guevara75@gmail.com>
2022-02-27 23:02:52 -0500

    AmigosMeat  Backend Setup

---
+++ src/main/java/com/carmenguevara/backend/AmigosmeatbackendApplication.java
@@ -0,0 +1,13 @@
+package com.carmenguevara.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosmeatbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosmeatbackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/carmenguevara/backend/models/Product.java
@@ -0,0 +1,57 @@
+package com.carmenguevara.backend.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class Product {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String code;
+	@Column
+	private String description;
+	@Column
+	private double cost;
+	@Column
+	private int qtyAvailable;
+	
+	
+	public String getCode() {
+		return code;
+	}
+	//We need this setter because we need to set our code for each product.
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	public int getQtyAvailable() {
+		return qtyAvailable;
+	}
+	public void setQtyAvailable(int qtyAvailable) {
+		this.qtyAvailable = qtyAvailable;
+	}
+	
+	
+	
+	
+	
+}
