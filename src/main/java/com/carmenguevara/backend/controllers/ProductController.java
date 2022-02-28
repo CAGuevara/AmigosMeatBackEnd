3213ab4e8f4ff731a78e59165fed08ea388b28b9
Carmen Guevara <carmen.guevara75@gmail.com>
2022-02-28 14:28:02 -0500

    EndPoints

--- src/main/java/com/carmenguevara/backend/controllers/ProductController.java
+++ src/main/java/com/carmenguevara/backend/controllers/ProductController.java
@@ -3,10 +3,13 @@ package com.carmenguevara.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.carmenguevara.backend.exceptions.ResourceNotFoundException;
 import com.carmenguevara.backend.models.Product;
 import com.carmenguevara.backend.repositories.ProductRepository;
 
@@ -20,7 +23,14 @@ public class ProductController {
 	@GetMapping("/allproduct")
 	public List<Product> getAllProduct(){
 		return productRepo.findAll();
+	}
 		
+	@GetMapping("product/{code}")
+		public ResponseEntity<Product> getProductByCode(@PathVariable String code){
+			Product product = productRepo.findById(code)
+									.orElseThrow(()-> new ResourceNotFoundException("Product Not Found"));
+					return ResponseEntity.ok(product);
+			
 	}
 	
 
