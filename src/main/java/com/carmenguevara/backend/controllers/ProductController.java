2bbaf7fff35fb1413ac21183225e757c5aaae1b1
Carmen Guevara <carmen.guevara75@gmail.com>
2022-02-28 19:08:54 -0500

    Working EndPoints

--- src/main/java/com/carmenguevara/backend/controllers/ProductController.java
+++ src/main/java/com/carmenguevara/backend/controllers/ProductController.java
@@ -4,23 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.carmenguevara.backend.exceptions.ResourceNotFoundException;
 import com.carmenguevara.backend.models.Product;
 import com.carmenguevara.backend.repositories.ProductRepository;
 
 @RestController
+//@CrossOrigin
 @RequestMapping("/api/v1/")
 public class ProductController {
 	
 	@Autowired
 	private ProductRepository productRepo;
 	
-	@GetMapping("/allproduct")
+	@GetMapping("/allproducts")
 	public List<Product> getAllProduct(){
 		return productRepo.findAll();
 	}
@@ -33,5 +36,14 @@ public class ProductController {
 			
 	}
 	
-
+	
+	
+	
+	@PostMapping("addProduct")
+	public Product newProduct (@RequestBody Product product) {
+		return productRepo.save(product);
+	}
+	
+	
+	
 }
--- src/main/java/com/carmenguevara/backend/repositories/ProductRepository.java
+++ src/main/java/com/carmenguevara/backend/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.carmenguevara.backend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.carmenguevara.backend.models.Product;
 @Repository
 public interface ProductRepository extends JpaRepository <Product,String> {
 
+	List<Product> findByDescription(String description);
+	
 }
